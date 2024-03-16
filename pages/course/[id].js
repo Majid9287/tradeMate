aaeb406089a0ffac6d6c0260a6452e3525745ea8
A.MAJID <103788155+Majid9287@users.noreply.github.com>
2024-03-16 15:33:12 -0700

    uppppp

--- pages/course/[id].js
+++ pages/course/[id].js
@@ -16,16 +16,35 @@ function ProductForm() {
 
   const [selectedChapter, setSelectedChapter] = useState(null);
   const [showCard, setShowCard] = useState(false);
-  const [selectedMaterial, setSelectedMaterial] = useState("video");  // Set default to "video"
+  const [selectedMaterial, setSelectedMaterial] = useState(""); // Set default to "video"
   const [videoUrl, setVideoUrl] = useState("");
   const [videoId, setVideoId] = useState("");
-
-  const handleMaterialClick = (materialType) => {
+  console.log("url", videoUrl);
+  const handleMaterialClick = (materialType, chapterId) => {
     setSelectedMaterial(materialType);
-    if (materialType === "video") {
-      setVideoUrl(course.chapters.find(chapter => chapter._id === selectedChapter)?.videos[0]?.video_url);
+  
+    // If the chapterId is provided, update the selected chapter
+    if (chapterId) {
+      setSelectedChapter(chapterId);
+      const selectedChapterData = course.chapters.find(chapter => chapter._id === chapterId);
+  
+      // Load the appropriate content based on the selected material type
+      switch (materialType) {
+        case "video":
+          setVideoUrl(selectedChapterData.videos[0]?.video_url);
+          break;
+        case "summary":
+          // Load summary content
+          break;
+        case "assignment":
+          // Load assignment content
+          break;
+        default:
+          break;
+      }
     }
   };
+  
 
   useEffect(() => {
     // Extract video ID from the entered URL
@@ -35,21 +54,32 @@ function ProductForm() {
     };
 
     // Set the video ID when a valid URL is entered
-    const id = extractVideoId(videoUrl);
-    setVideoId(id);
+    if (videoUrl) {
+      const id = extractVideoId(videoUrl);
+      setVideoId(id);
+    }
   }, [videoUrl]);
-  
+
   useEffect(() => {
-
-
-    
-  if(course){
-    // Set the default selected chapter to the first chapter
-    setSelectedChapter(course.chapters[0]?._id);
-
-    // Set the default video URL
-    setVideoUrl(course.chapters[0]?.videos[0]?.video_url);}
+    if (course) {
+      // Set the default selected chapter to the first chapter
+      setSelectedChapter(course.chapters[0]?._id);
+  
+      // Check for the availability of each type of material in the selected chapter
+      const selectedChapterData = course.chapters.find((chapter) => chapter._id === selectedChapter);
+      if (selectedChapterData) {
+        if (selectedChapterData.videos.length > 0) {
+          setSelectedMaterial("video");
+          setVideoUrl(selectedChapterData.videos[0]?.video_url);
+        } else if (selectedChapterData.summary.length > 0) {
+          setSelectedMaterial("summary");
+        } else if (selectedChapterData.assignments.length > 0) {
+          setSelectedMaterial("assignment");
+        }
+      }
+    }
   }, [course]);
+  
 
   useEffect(() => {
     // Fetch data for the specific entry to be updated
@@ -60,12 +90,11 @@ function ProductForm() {
         try {
           const response = await fetch(`/api/course/get-by-id?id=${id}`);
           const data = await response.json();
-          console.log(data.course)
+          console.log(data.course);
           if (data) {
             setCourse(data.course);
           } else {
             console.error("not found");
-            
           }
         } catch (error) {
           console.error("Error fetching data:", error);
@@ -86,7 +115,6 @@ function ProductForm() {
                   {selectedMaterial === "video" && videoId && (
                     <iframe
                       width="100%"
-                     
                       src={`https://www.youtube.com/embed/${videoId}`}
                       title="Video"
                       className="video-iframe h-[100%] md:h-[500px]"
@@ -94,47 +122,79 @@ function ProductForm() {
                     ></iframe>
                   )}
                   {selectedMaterial === "summary" && (
-                    <div className={styles.list} dangerouslySetInnerHTML={{ __html: course.chapters.find(chapter => chapter._id === selectedChapter)?.summary[0]?.content }} />
+                    <div
+                      className={styles.list}
+                      dangerouslySetInnerHTML={{
+                        __html: course.chapters.find(
+                          (chapter) => chapter._id === selectedChapter
+                        )?.summary[0]?.content,
+                      }}
+                    />
                   )}
                   {selectedMaterial === "assignment" && (
-                    <div className={styles.list} dangerouslySetInnerHTML={{ __html: course.chapters.find(chapter => chapter._id === selectedChapter)?.assignments[0]?.content }}></div>
+                    <div
+                      className={styles.list}
+                      dangerouslySetInnerHTML={{
+                        __html: course.chapters.find(
+                          (chapter) => chapter._id === selectedChapter
+                        )?.assignments[0]?.content,
+                      }}
+                    ></div>
                   )}
                 </>
               )}
             </div>
           </div>
           <div className="md:w-1/4 w-full md:m-2">
-            {course?course.chapters.map((chapter) => (
-              <div key={chapter._id} className=" my-2 bg-gray-200">
-                <span
-                  className="p-4 text-2xl font-bold flex content-center text-center justify-between "
-                  onClick={() => setSelectedChapter(chapter._id)}
-                >
-                  {chapter.title}
-                  {selectedChapter === chapter._id ? (
-                    <IoChevronUp className="mt-2" />
-                  ) : (
-                    <IoChevronDown className="mt-2" />
-                  )}
-                </span>
-                {selectedChapter === chapter._id && (
-                  <div className="px-4 md:text-2xl pb-4">
-                    {chapter.videos.length>0&&
-                    <div className="flex border-t-2 border-black" onClick={() => handleMaterialClick("video")}>
-                     <FaRegFileVideo className="mt-1" /> <span >{chapter.videos[0].title}</span>
-                    </div>}
-                    {chapter.summary.length>0&&
-                    <div className="flex border-y-2 border-black" onClick={() => handleMaterialClick("summary")}>
-                      <MdOutlineSummarize className="mt-1" /> {chapter.summary[0]?.title}
-                    </div>}
-                    {chapter.assignments.length>0&&
-                    <div className="flex border-b-2 border-black" onClick={() => handleMaterialClick("assignment")}>
-                      <MdAssignment className="mt-1" /> {chapter.assignments[0]?.title}
-                    </div>}
-                  </div>
-                )}
+          {course
+  ? course.chapters.map((chapter) => (
+      <div key={chapter._id} className="my-2 bg-gray-200">
+        <span
+          className="p-4 text-2xl font-bold flex content-center text-center justify-between"
+          onClick={() => setSelectedChapter(chapter._id)}
+        >
+          {chapter.title}
+          {selectedChapter === chapter._id ? (
+            <IoChevronUp className="mt-2" />
+          ) : (
+            <IoChevronDown className="mt-2" />
+          )}
+        </span>
+        {selectedChapter === chapter._id && (
+          <div className="px-4 md:text-2xl pb-4">
+            {chapter.videos.length > 0 && (
+              <div
+                className="flex border-t-2 border-black"
+                onClick={() => handleMaterialClick("video", chapter._id)}
+              >
+                <FaRegFileVideo className="mt-1" />{" "}
+                <span>{chapter.videos[0].title}</span>
               </div>
-            )):""}
+            )}
+            {chapter.summary.length > 0 && (
+              <div
+                className="flex border-y-2 border-black"
+                onClick={() => handleMaterialClick("summary", chapter._id)}
+              >
+                <MdOutlineSummarize className="mt-1" />{" "}
+                {chapter.summary[0]?.title}
+              </div>
+            )}
+            {chapter.assignments.length > 0 && (
+              <div
+                className="flex border-b-2 border-black"
+                onClick={() => handleMaterialClick("assignment", chapter._id)}
+              >
+                <MdAssignment className="mt-1" />{" "}
+                {chapter.assignments[0]?.title}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    ))
+  : ""}
+
           </div>
         </div>
       </div>
